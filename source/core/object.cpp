5c86ab2a831482d143c117463d1f97c23114c040
Denny Lindberg <DennyLindberg@users.noreply.github.com>
2023-07-30 22:14:26 +0200

    Object.h/.cpp cleanup

--- source/core/object.cpp
+++ source/core/object.cpp
@@ -1,3 +1,68 @@
 #include "object.h"
 
 ObjectPool<Object, OBJECTTYPE_OBJECT> Object::Pool;
+
+void Object::DetachFromParent()
+{
+	if (Object* p = parent.Get())
+	{
+		p->RemoveChild(Pool.GetWeakPtr(poolId));
+	}
+}
+
+void Object::AddChild(WeakObjectPtr<Object> newChild)
+{
+	if (Object* obj = newChild.Get())
+	{
+		obj->parent = Pool.GetWeakPtr(poolId);
+
+		for (WeakObjectPtr<Object>& child : children)
+		{
+			if (child == newChild)
+				return; // already a child
+		}
+
+		children.push_back(newChild);
+	}
+}
+
+void Object::RemoveChild(WeakObjectPtr<Object> child)
+{
+	if (Object* obj = child.Get())
+	{
+		obj->parent.Clear();
+	}
+
+	for (size_t i = 0; i < children.size(); ++i)
+	{
+		if (children[i] == child)
+		{
+			children[i].Clear();
+		}
+	}
+}
+
+void Object::AddComponent(GenericWeakObjectPtr weakPtr)
+{
+	for (const GenericWeakObjectPtr& child : components)
+	{
+		if (child == weakPtr)
+			return; // already added
+	}
+
+	components.push_back(weakPtr);
+}
+
+glm::mat4 Object::ComputeWorldMatrix()
+{
+	glm::mat4 mat = transform.Matrix();
+
+	Object* p = parent.Get();
+	while (p)
+	{
+		mat = p->transform.Matrix() * mat;
+		p = p->parent.Get();
+	}
+
+	return mat;
+}
--- source/core/object.h
+++ source/core/object.h
@@ -9,102 +9,31 @@ public:
 	friend class ObjectPool<Object, OBJECTTYPE_OBJECT>;
 	static ObjectPool<Object, OBJECTTYPE_OBJECT> Pool;
 
-	Object() {}
-	~Object() {}
-
-	void Initialize() {}
-	void Destroy() {}
-
-	Transform transform;
-
-	WeakObjectPtr<Object> GetParent() const { return parent; }
-
-	void DetachFromParent()
-	{
-		if (Object* p = parent.Get())
-		{
-			p->RemoveChild(Pool.GetWeakPtr(poolId));
-		}
-	}
-
-	void AddChild(WeakObjectPtr<Object> newChild)
-	{
-		if (Object* obj = newChild.Get())
-		{
-			obj->parent = Pool.GetWeakPtr(poolId);
-
-			for (WeakObjectPtr<Object>& child : children)
-			{
-				if (child == newChild)
-					return; // already a child
-			}
-
-			children.push_back(newChild);
-		}
-	}
-
-	void RemoveChild(WeakObjectPtr<Object> child)
-	{
-		if (Object* obj = child.Get())
-		{
-			obj->parent.Clear();
-		}
-
-		for (size_t i=0; i<children.size(); ++i)
-		{
-			if (children[i] == child)
-			{
-				children[i].Clear();
-			}
-		}
-	}
-
-	void AddComponent(GenericWeakObjectPtr weakPtr)
-	{
-		for (const GenericWeakObjectPtr& child : components)
-		{
-			if (child == weakPtr)
-				return; // already added
-		}
-
-		components.push_back(weakPtr);
-	}
-
-	template<class T>
-	void AddComponent(WeakObjectPtr<T> weakPtr)
-	{
-		AddComponent(GenericWeakObjectPtr(weakPtr));
-	}
-
-	glm::mat4 ComputeWorldMatrix()
-	{
-		glm::mat4 mat = transform.Matrix();
-
-		Object* p = parent.Get();
-		while (p)
-		{
-			mat = p->transform.Matrix() * mat;
-			p = p->parent.Get();
-		}
-
-		return mat;
-	}
-
-	// TODO: Scene graph must check generic objects against known object pools
-	//void CleanupDestroyedComponents(std::function<void(const GenericWeakObjectPtr& child)> IsInvalidChild)
-	//{
-	//	children.erase(
-	//		std::remove_if(children.begin(), children.end(), IsInvalidChild),
-	//		children.end()
-	//	);
-	//}
-
-public:
 	std::string name = "Object";
+	Transform transform;
 
 protected:
 	ObjectId poolId = 0;
 	WeakObjectPtr<Object> parent;
 	std::vector<WeakObjectPtr<Object>> children;
 	std::vector<GenericWeakObjectPtr> components;
+
+public:
+	Object() {}
+	~Object() {}
+
+	void Initialize() {}
+	void Destroy() {}
+
+	WeakObjectPtr<Object> GetParent() const { return parent; }
+
+	void DetachFromParent();
+
+	void AddChild(WeakObjectPtr<Object> newChild);
+
+	void RemoveChild(WeakObjectPtr<Object> child);
+
+	void AddComponent(GenericWeakObjectPtr weakPtr);
+
+	glm::mat4 ComputeWorldMatrix();
 };
